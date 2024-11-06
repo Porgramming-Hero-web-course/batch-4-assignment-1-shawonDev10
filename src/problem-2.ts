593e0cb842cda2ec2fae40f555ee6251404b3b72
Shawon Kumar Sarkar <shawonkumarsarkar.bd@gmail.com>
2024-11-06 23:02:53 +0600

    problem-2 complete

---
+++ src/problem-2.ts
@@ -0,0 +1,11 @@
+type numArray = number[];
+
+function removeDuplicates(values: numArray): numArray {
+    const duplicatesRemoved: numArray = [];
+    values.forEach((value) => {
+        if (!duplicatesRemoved.includes(value)) {
+            duplicatesRemoved.push(value)
+        };
+    });
+    return duplicatesRemoved;
+};
