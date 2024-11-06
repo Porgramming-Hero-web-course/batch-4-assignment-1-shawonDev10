7b81ef4239bdf22f066a7458dee8433c21e3fc85
Shawon Kumar Sarkar <shawonkumarsarkar.bd@gmail.com>
2024-11-07 00:48:10 +0600

    problem-4 complete

---
+++ src/problem-4.ts
@@ -0,0 +1,19 @@
+type Circle = {
+    shape: "circle",
+    radius: number
+};
+
+type Rectangle = {
+    shape: "rectangle",
+    width: number,
+    height: number
+};
+
+type ShapeMustBe = Circle | Rectangle;
+
+function calculateShapeArea(obj: ShapeMustBe): number {
+    if (obj.shape === "circle") {
+        return Math.PI * (obj.radius * obj.radius);
+    }
+    return obj.width * obj.height;
+};
